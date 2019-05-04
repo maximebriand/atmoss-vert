082ef5e5b8e1570a2c1a68a72207dc3cd20852f8
Maxime Briand <contact@maximebriand.fr>
2019-05-04 21:14:19 +0200

    limit js for before after to chantier pages

--- wp-content/themes/atmoss-vert/functions.php
+++ wp-content/themes/atmoss-vert/functions.php
@@ -131,11 +131,14 @@ function atmoss_vert_scripts() {
 	wp_enqueue_style( 'atmoss-vert-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'atmoss-vert-slick', get_template_directory_uri() . '/js/slick.min.js', array('jquery'), '20151215', true );
-	wp_enqueue_script( 'atmoss-vert-before-after', get_template_directory_uri() . '/js/before-after.js', array('jquery'), '20151215', true );
 	wp_enqueue_script( 'atmoss-vert-main', get_template_directory_uri() . '/js/main.js', array('jquery'), '20151215', true );
 
 	wp_enqueue_script( 'atmoss-vert-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	if (is_singular( 'chantiers' )) {
+        wp_enqueue_script( 'atmoss-vert-before-after', get_template_directory_uri() . '/js/before-after.js', array('jquery'), '20151215', true );
+
+    }
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
